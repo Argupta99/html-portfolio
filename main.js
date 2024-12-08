79341ca37af694819a60cda599caca0c64a32d07
Argupta99 <178310155+Argupta99@users.noreply.github.com>
2024-12-08 12:35:37 +0530

    open/close menu

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
+const sideMenu = document.querySelector('#sideMenu');
+function openMenu () {
+    sideMenu.style.transform = 'translateX(-16rem)';
+}
 
+function closeMenu() {
+    sideMenu.style.transform = 'translateX(16rem)';
+}
 
   // Select elements
 const themeToggle = document.getElementById('themeToggle');
